ac44adc937e0e1b7fe6e136dde901ee3d68ca1cb
talibarbosa-hub <talita.bab@gmail.com>
2019-10-28 13:30:28 -0300

    resolve exercicios 1 e 2

--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -1,14 +1,26 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
 
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [] remover repetição de código
-// [] anonymous function
+// [x] remover repetição de código
+// [x] anonymous function
 // [] arrow function
 // [] template string
+
+const body = document.querySelector("body")
+const btn = document.getElementById("trocaCor")
+
+btn.addEventListener("click", ()=> {
+    const r = geraInteiro(255)
+    const g = geraInteiro(255)
+    const b = geraInteiro(255)
+    body.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+    
+})
+geraInteiro = num=> parseInt(Math.random()*num)
